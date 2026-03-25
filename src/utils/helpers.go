737cfe6373be31fec1c0ceab39253140c0ecccee
assassin-create <262694275+assassin-create@users.noreply.github.com>
2026-03-26 03:48:03 +1000

    Added CSV reading and writing functionality

---
+++ src/utils/helpers.go
@@ -0,0 +1,69 @@
+package helpers
+
+import (
+	"encoding/csv"
+	"errors"
+	"io"
+	"os"
+)
+
+func ReadCSV(filePath string) ([][]string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+
+	return records, nil
+}
+
+func WriteCSV(filePath string, records [][]string) error {
+	if len(records) == 0 {
+		return errors.New("empty records provided")
+	}
+
+	file, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	for _, record := range records {
+		if err := writer.Write(record); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func FileExists(filePath string) bool {
+	_, err := os.Stat(filePath)
+	return !os.IsNotExist(err)
+}
+
+func CopyFile(src, dst string) error {
+	sourceFile, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer sourceFile.Close()
+
+	destinationFile, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer destinationFile.Close()
+
+	_, err = io.Copy(destinationFile, sourceFile)
+	return err
+}
